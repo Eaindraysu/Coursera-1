696758853130e1b7cd348c13bd59d5bbff5ee8f9
Eaindraysu <68538119+Eaindraysu@users.noreply.github.com>
2020-08-01 10:40:10 +0630

    Update script.js

--- Module-5-solution/js/script.js
+++ Module-5-solution/js/script.js
@@ -117,9 +117,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = 
-           chosenCategoryShortName(categories,
-                                   homeHtml,
-                                   buildAndShowHomeHTML);
+           chosenCategoryShortName;
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
